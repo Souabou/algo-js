585ecd87baba271b75f31dfa34b752f1f27f9203
Bryan Gustin <soumaya.abouda@gmail.com>
2020-10-16 16:40:42 +0200

    starting exo 4_5

---
+++ js/exo4_5.js
@@ -0,0 +1,13 @@
+const readlineSync = require("readline-sync");
+
+let a = readlineSync.question('first point position: ');
+let b = readlineSync.question('last point position: ');
+let x = x1 - x2;
+let y = y1 - y2;
+
+
+function calDistance() {
+    let c = Math.sqrt( a*a + b*b );
+}
+
+console.log(calDistance(c));
